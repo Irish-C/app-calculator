68af302439603d5da224de282c71a6439095b57c
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-05-10 23:05:11 +0800

    Write --code for try again window

--- app_calculator.py
+++ app_calculator.py
@@ -17,15 +17,15 @@ def divide(a, b):
 
 # Define main function
 def main():
-    sg.theme("DarkTeal10")
+    sg.theme("DarkAmber")
     # Define the math_operations window's contents
     layout = [
-        [sg.Text('Choose a math operation:')],
+        [sg.Text('Select a math operation:')],
         [sg.Radio('Add', 'RADIO1', key='add'), sg.Radio('Subtract', 'RADIO1', key='subtract'),
          sg.Radio('Multiply', 'RADIO1', key='multiply'), sg.Radio('Divide', 'RADIO1', key='divide')],
         [sg.Frame('Inputs', [
-            [sg.Text('Enter the first number:'), sg.InputText(key='a')],
-            [sg.Text('Enter the second number:'), sg.InputText(key='b')]])],
+            [sg.Text('Enter the first number:'), sg.InputText(key='first')],
+            [sg.Text('Enter the second number:'), sg.InputText(key='second')]])],
         [sg.Button('Submit'), sg.Button('Exit')]
     ]
 
@@ -36,12 +36,12 @@ def main():
     while True:
         event, values = window.read()
         if event in (None, 'Exit'):
-            break
+            window.close()
 
         # TRY calling math operations
         try:
-            num1 = float(values['a'])
-            num2 = float(values['b'])
+            num1 = float(values['first'])
+            num2 = float(values['second'])
             # If "addition", call addition function
             if values['add']:
                 result = add(num1, num2)
@@ -60,21 +60,36 @@ def main():
             # Else, user did not choose any math operations
             else:
                 sg.popup("Please select a math operation.", title="Math Operation Error")
+                continue
 
             # Create Pop-Up to display result
-            sg.popup(f'The result is: {result}', title='Result', button_type=sg.BUTTON_OK)
- 
+            sg.popup(f'The result is: {result}', title='Result', button_type=sg.POPUP_BUTTONS_OK, keep_on_top=True)
+            try_again()
         # exception for ZeroDivisionError using Pop-up
         except ValueError:
-            sg.popup('Invalid input. Please enter valid numbers.', title='Error')
+            sg.popup('Invalid input. Please enter valid numbers.', title='Math Operation Error', keep_on_top=True)
+            try_again()
+            break
         # exception for ValueError using Pop-up
         except ZeroDivisionError:
-            sg.popup("Cannot divide by zero. Please enter a different second number.", title="Error")
+            sg.popup("Cannot divide by zero. Please enter a different second number.", title=" Zero Division Error", keep_on_top=True)
+            try_again()
+            break
 
-            # Define the try_again window's contents
-            # Display try_again window
-                # TRY Ask the user wants to try again or not
-                # If "yes", repeat start loop
-                # If "no", program will exit or if window is closed
+def try_again():
+    # Define the try_again window's contents
+    layout_try_again = [
+        [sg.Text('Do you want to try again?')],
+        [sg.Button('Yes'), sg.Button('No')]
+    ]            
+    # Display try_again window
+    window_try_again = sg.Window('Try Again', layout_try_again)
+    event, values = window_try_again.read()
+    if event == 'Yes':
+        main()
+    if event == None or event == 'No':
+        sg.popup('Exiting application', title='Exit')
+        window_try_again.close()
 
-# Close the window
+if __name__ == '__main__':
+    main()
